0a3e7aa20d93b4d659bc7ad7ad788e27cf99f596
Karen Javadyan <karen@teamable.com>
2018-11-10 20:32:22 +0400

    first test case

--- pizza_app/tests.py
+++ pizza_app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from pizza_app.models import Pizza
+
+class PizzaTests(APITestCase):
+    def test_create_pizza(self):
+        """
+        Ensure we can create a new pizza object.
+        """
+        url = reverse('pizzas-list')
+        data = {'name': 'Quattro Formaggio'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Pizza.objects.count(), 1)
+        self.assertEqual(Pizza.objects.get().name, 'Quattro Formaggio')
